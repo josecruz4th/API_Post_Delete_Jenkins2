ebc4daec19d8ba02b75d502ead0c66ae9b5e3b25
Jose Humberto Cruz Valencia <jose.cruz@4thsource.com>
2020-02-06 17:26:29 -0600

    second commit

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -16,6 +16,7 @@ namespace UnitTestProject1
                 string strStringToVal = this.dataToPost("Jose", "12", "13");
                 string strStringToDelete = this.dataToDelete(strStringToVal);
                 Console.WriteLine("DELETE RESPONSE IS: " + strStringToDelete);
+                Assert.IsTrue(true, strStringToDelete);
 
             }
             catch (WebException ex)
