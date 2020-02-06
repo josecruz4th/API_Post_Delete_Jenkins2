9a77d27b495e0d9a916aa741d29321e2e8a43911
Jose Humberto Cruz Valencia <jose.cruz@4thsource.com>
2020-02-06 17:14:26 -0600

    first

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1:Post_Delete_Classes
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            try
+            {
+
+                string strStringToVal = this.dataToPost("Jose", "12", "13");
+                string strStringToDelete = this.dataToDelete(strStringToVal);
+                Console.WriteLine("DELETE RESPONSE IS: " + strStringToDelete);
+
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
